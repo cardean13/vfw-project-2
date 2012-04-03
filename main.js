30efe4f42d554addf7a7db7f3f09acfa42557ea6
Yusef Hassan <Bioassassin@trusted.shearers.com>
2012-04-03 09:05:54 -0400

    Unsure if code works yet, finished item list though.

--- main.js
+++ main.js
@@ -6,6 +6,8 @@
 
 //wait till DOM is ready
 window.addEventListener("DOMcontentloaded", function(){
+	
+	alert(localStorage.value(0));
 	//get element by ID function
 	function e(x){
 		var id = document.getElementById(x);
@@ -28,31 +30,81 @@ window.addEventListener("DOMcontentloaded", function(){
 		selectoptgroup.appendChild(makeSelect);
 	}
 	
+	//find value of radio button
+	function getSelectedRadio(){
+		var radio = document.form[0].sex;
+		for(var i=0; i<radios.length; i++){
+			if(radios[i].checked){
+				sexValue = radios[i].value;
+			}
+		}
+	}
+	
+	function getCheckBoxValue(){
+		if(e("reliable").checked){
+			reliableValue = e("reliable").value;
+		}else{
+			reliableValue = "No"
+		}
+	}	
+	
+	function getCheckBoxValue(){
+		if(e("job").checked){
+			jobValue = e("job").value;
+		}else{
+			jobValue = "No"
+		}
+	}
+	
+	function getCheckBoxValue(){
+		if(e("replace").checked){
+			replaceValue = e("replace").value;
+		}else{
+			replaceValue = "No"
+		}
+	}
+	
+	function getCheckBoxValue(){
+		if(e("trust").checked){
+			trustValue = e("trust").value;
+		}else{
+			trustValue = "No"
+		}
+	}
 	function saveData(){
 		var id = Math.floor(Math.random()*123456);
 		//gather form field data, store in object, object contains array with form label and input value
+		getSelectedRadio();
+		getCheckBoxValue();		
 		var item = {};
 			item.fname =["First Name:", e("fname").value];
 			item.lname =["Last Name:", e("lname").value];
-			//sex
-			item.sex =["First Name", e("fname").value];
+			item.sex =["Sex", sexValue];
 			item.age =["Age", e("ageRange").value];
-			item.reliable =["Is the borrower reliable?", e("reliable").value];
-			item.job =["Do they have a job?", e("job").value];
-			item.replace =["If broken, could they replace it?", e("replace").value];
-			item.trust =["Do you fully trust them?", e("trust").value];
+			item.reliable =["Is the borrower reliable?", reliableValue];
+			item.job =["Do they have a job?", jobValue];
+			item.replace =["If broken, could they replace it?", replaceValue];
+			item.trust =["Do you fully trust them?", trustValue];
 			item.dname =["Disc Name", e("dname").value];
 			item.value =["Value", e("value").value];
 			item.ldate =["Date Lent", e("ldate").value];
 			item.rdate =["Expected Return Date", e("rdate").value];
 			item.comments =["Anymore Information?", e("comments").value];
 
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Information Logged");
 	}
 	
 	
 	
 	// variable defaults
-	var typeOfMedia = ["--Movies--", "DVD", "VHS", "Blu-Ray", "--Games--", "Xbox 360", "PS3", "Wii" ]
+	var typeOfMedia = ["--Movies--", "DVD", "VHS", "Blu-Ray", "--Games--", "Xbox 360", "PS3", "Wii" ],
+		sexValue,
+		reliableValue = "No",
+		jobValue = "No",
+		replaceValue = "No",
+		trustValue = "No"
+	;
 	wheelHouse();
 	
 	//links and submit button
